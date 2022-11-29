25d57b6f4191e817b2f7b77e4244744dcfc8a4c3
DarkFelipe <fr.fisica@gmail.com>
2022-11-29 12:17:34 -0300

    Estudo_de_Classes     new file:   Primeiro-projeto-java/.vscode/settings.json 	new file:   Primeiro-projeto-java/README.md 	new file:   Primeiro-projeto-java/bin/App.class 	new file:   Primeiro-projeto-java/src/App.java 	new file:   java-anatomia-classes/.vscode/settings.json 	new file:   java-anatomia-classes/README.md 	new file:   java-anatomia-classes/bin/MinhaCLasse.class 	new file:   java-anatomia-classes/src/MinhaCLasse.java

---
+++ java-anatomia-classes/src/MinhaCLasse.java
@@ -0,0 +1,14 @@
+
+public class MinhaCLasse {
+    // Void (Não recebede nada, apenas executa); main (método principal); (String é um vetor); Args são os argumentos
+public static void main (String [] args) {
+        
+    System.out.print("Olá turma, sejam bem-vindos!");
+    
+} 
+
+}
+
+// se a classe for executável, para iniciar o projeto de forma dependente, utilizamos o método main
+
+final String BR = "Brasil"; //quando uso FINAL e o nome da variável é maiúscula, ela nunca pode ser alterada.
