770079f8b4139f56a2fd375d27a5b5a034dfe945
DarkFelipe <fr.fisica@gmail.com>
2022-12-07 15:01:10 -0300

    modified:   Projeto-smart-tv/projeto-smart-tv/bin/SmartTv.class 	modified:   Projeto-smart-tv/projeto-smart-tv/bin/Usuario.class 	modified:   Projeto-smart-tv/projeto-smart-tv/src/SmartTV.java 	modified:   Projeto-smart-tv/projeto-smart-tv/src/Usuario.java

--- Projeto-smart-tv/projeto-smart-tv/src/Usuario.java
+++ Projeto-smart-tv/projeto-smart-tv/src/Usuario.java
@@ -2,6 +2,10 @@ public class Usuario {
    
     public static voide main(String [] args) throws Exception {
 
-        SmartTV
+        SmartTV smartTV = new SmartTV();
+
+        System.out.println(smartTV.ligada);
+        System.out.println(smartTV.canal);
+        System.out.println(smartTV.volume);
     }
 }
